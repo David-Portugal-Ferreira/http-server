c5553d0b5f6e249ce00299831000de37467bbb7f
David Portugal Ferreira <davidportugal44@gmail.com>
2026-03-29 00:59:53 +0000

    Add express to the project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import http from 'node:http';
-import fs from 'node:fs'
+//import http from 'node:http';
+//import fs from 'node:fs'
 
+const fs = require('fs')
+
+
+/*
 const server = http.createServer((req, res) => {
     console.log('Hello World!')
     console.log(`Request URL ${req.url}`)
@@ -48,6 +52,63 @@ const server = http.createServer((req, res) => {
     }
     
 })
-
-
 server.listen(8000);
+*/
+
+
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        res.write(data);
+        res.end();
+    })
+});
+
+app.get('/about-me', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./about.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        res.write(data);
+        res.end();
+    })
+});
+app.get('/contact-me', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./contact-me.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        res.write(data);
+        res.end();
+    })
+});
+
+app.use((req, res) => {
+    fs.readFile('404.html', (err, data) => {
+        if(err) {
+            console.log(err)
+            return;
+        }
+        res.write(data);
+        res.end();
+    })
+})
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+})
